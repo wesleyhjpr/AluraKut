32544136cc8ab8a7c15e143f6e4f35867c846ebb
wesley <wesleyhjpr@gmail.com>
2021-07-19 14:22:21 -0300

    parte 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/components/lib/AluraKutCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  // Reset css
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #d9e6f6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,68 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/components/lib/AluraKutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/profileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(proriedades) {
+  return (
+    <Box>
+      <img src={`https://github.com/${proriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuario = 'wesleyhjpr';
+  const pessoasFavoritas =
+    [
+      'Th3Daniel',
+      'RaFaElMOW',
+      'udison',
+      'czucherato',
+      'RodolphoTrinca'
+    ];
+  return (
+    <>
+      <AlurakutMenu githubUser={usuario} />
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea" > */}
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuario} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet/>
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {
+                pessoasFavoritas.map((githubUser) => {
+                  return (
+                    <li>
+                      <a href={`/users/${githubUser}`} key={githubUser}>
+                        <img src={`https://github.com/${githubUser}.png`} />
+                        <span>{githubUser}</span>
+                      </a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
